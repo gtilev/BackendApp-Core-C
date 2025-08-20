22aa133f63174912af0513e361b7a6d2640e07f8
cvetan7406 <33251947+cvetan7406@users.noreply.github.com>
2025-08-20 17:53:42 +0300

    AS
    ASD

---
+++ app/services/auditalgo/__init__.py
@@ -0,0 +1,11 @@
+class CalculateValues  :
+    def __init__():
+        pass
+    
+
+
+    def is_under_thirty(check:bool):
+        if check :
+            return True
+        else:
+            return False
